7e599aacb3f2ad22b4f4c979f7913f5ee8cecc05
AyushiNamdev <ayushinamdev2@gmail.com>
2022-02-16 16:33:24 -0500

    dog years program

---
+++ dogyears.js
@@ -0,0 +1,10 @@
+onst myAge = 24;
+//earlyyears
+let earlyYears = 2;
+earlyYears = earlyYears*10.5;
+let laterYears = myAge - 2;
+//now we have subracted the early age
+laterYears *=4;
+const myAgeInDogYears = earlyYears+laterYears;
+const myName= 'AyushiNamdev'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge}years old in human years which is ${myAgeInDogYears} years old in dog years.`)
