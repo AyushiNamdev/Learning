b1902d8d70af1adf8928dae1d89819a1b28f7b21
AyushiNamdev <ayushinamdev2@gmail.com>
2022-02-17 13:04:42 -0500

    variale

--- variablejs.js
+++ variablejs.js
@@ -2,3 +2,7 @@ var favoriteFood = 'pizza';
 var numOfSlices = 8;
 console.log(favoriteFood);
 console.log(numOfSlices);
+
+const myName = 'Ayushi';
+const myCity = 'Mumbai';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
